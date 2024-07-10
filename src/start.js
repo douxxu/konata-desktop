f07adfa1d8e112de40352358bf36b6e15461426d
Douxx <136928935+douxxu@users.noreply.github.com>
2024-07-10 02:35:25 +0200

    Create start.js

---
+++ src/start.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process');
+const path = require('path');
+
+const mainScriptPath = path.join(__dirname, 'main.js');
+
+const electronProcess = spawn('electron', [mainScriptPath]);
+
+electronProcess.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+electronProcess.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+electronProcess.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
+electronProcess.on('error', (err) => {
+  console.error('Failed to start Electron process:', err);
+});
