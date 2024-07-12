d482b40c93ba2583e939210e35b8e351d6ab95f4
Douxx <136928935+douxxu@users.noreply.github.com>
2024-07-12 19:52:22 +0100

    Add files via upload

---
+++ src/run.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const { spawn } = require('child_process');
+
+const electronPath = path.join(__dirname, 'konata-desktop'); 
+
+const child = spawn('electron', [electronPath], {
+  stdio: 'inherit'
+});
+
+child.on('close', (code) => {
+  console.log(`Child process exited with code ${code}`);
+});
